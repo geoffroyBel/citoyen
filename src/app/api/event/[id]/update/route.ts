5e38d5aafe5bd9e876418ce732b54b71f253933b
Theo-Maes <theo27670mae@gmail.com>
2024-05-31 10:28:00 +0200

    adding image management on  update event

--- src/app/api/event/[id]/update/route.ts
+++ src/app/api/event/[id]/update/route.ts
@@ -1,9 +1,37 @@
 import { prisma } from "@/libs";
 import { Event } from "@prisma/client";
+import { unlink, writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
+import { join } from "path";
 
 export async function PATCH(req: Request, params: { params: { id: string } }) {
   const id = Number.parseInt(params.params.id);
+  const data = await req.formData();
+
+  const file: File | null = data.get("file") as unknown as File;
+  let path: string = "";
+
+  if (file) {
+    try {
+      const eventFind: Event | null = await prisma.event.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      if (eventFind) {
+        path = join(process.cwd(), "public", eventFind.image);
+      }
+      await unlink(path);
+    } catch (error) {
+      return NextResponse.json({ erreur: error }, { status: 500 });
+    }
+
+    path = join(process.cwd(), "public", "event", Date.now() + file.name);
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    await writeFile(path, buffer);
+  }
+
   const {
     name,
     description,
@@ -15,7 +43,7 @@ export async function PATCH(req: Request, params: { params: { id: string } }) {
     latitude,
     longitude,
     categoryId,
-  }: Event = await req.json();
+  } = Object.fromEntries(data.entries()) as unknown as Event;
 
   const updateData: Partial<Event> = {
     ...(name && { name }),
@@ -28,6 +56,7 @@ export async function PATCH(req: Request, params: { params: { id: string } }) {
     ...(latitude && { latitude }),
     ...(longitude && { longitude }),
     ...(categoryId && { categoryId }),
+    image: file ? path.replace(join(process.cwd(), "public"), "") : undefined,
   };
 
   try {
--- src/app/api/event/create/route.ts
+++ src/app/api/event/create/route.ts
@@ -18,23 +18,34 @@ export async function POST(req: NextRequest) {
   const path = join(process.cwd(), "public", "event", Date.now() + file.name);
   await writeFile(path, buffer);
   try {
-    const { name } = Object.fromEntries(data.entries()) as unknown as Event;
+    const {
+      name,
+      description,
+      endingDate,
+      startingDate,
+      address,
+      city,
+      zipCode,
+      latitude,
+      longitude,
+      categoryId,
+    } = Object.fromEntries(data.entries()) as unknown as Event;
 
     const newEvent: Event = await prisma.event.create({
       data: {
         name: name,
-        description: "description",
-        endingDate: "2025-12-12T00:00:00.000Z",
-        startingDate: "2022-12-12T00:00:00.000Z",
-        address: "address",
-        city: "city",
-        zipCode: "zipCode",
-        latitude: "latitude",
-        longitude: "longitude",
+        description: description,
+        endingDate: endingDate,
+        startingDate: startingDate,
+        address: address,
+        city: city,
+        zipCode: zipCode,
+        latitude: latitude,
+        longitude: longitude,
         image: path.replace(join(process.cwd(), "public"), ""),
         category: {
           connect: {
-            id: 1,
+            id: categoryId,
           },
         },
       },
@@ -43,6 +54,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ newEvent }, { status: 201 });
   } catch (error) {
     if (file) await unlink(path);
+    console.error(error);
     return NextResponse.json({ error: error }, { status: 400 });
   }
 }
